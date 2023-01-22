13556247b2e97c830a99ecf7a6c59db621f91730
Vivian Xu <hi@vivianxu.com>
2023-01-22 22:44:40 +1100

    Connected to API

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,17 +1,40 @@
-import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+import { HiMagnifyingGlass } from "react-icons/hi2";
 
 const Search = () => {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState(null);
+
+  const apiKey = `6e3c80dd3f1ec415fbe9f7176142a50c`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    fetch(apiUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+      });
+  };
+
+  const cityHandler = (e) => {
+    setCity(e.target.value);
+  };
+
   return (
     <div className="relative">
-      <form action="">
-        <div className="relative rounded-md">
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-            <FaSearch size={20} className="text-gray-500" />
+      <form onSubmit={submitHandler}>
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center">
+            <HiMagnifyingGlass className="text-gray-500" />
           </div>
           <input
+            onChange={cityHandler}
             type="text"
             placeholder="e.g. Sydney"
-            className="border-solid border-2 rounded-2xl bg-white  border-teal-500 mt-4 py-2 px-4 block w-full leading-5 pl-10"
+            className="border-solid border-2 rounded-2xl bg-white  border-teal-500 mt-4 py-2 px-3 block w-full leading-5 pl-10"
           />
         </div>
       </form>
