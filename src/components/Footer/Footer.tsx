2f4b547cb5ed54885f60cc38369c9806a5dd9a35
Vivian Xu <hi@vivianxu.com>
2023-03-12 14:21:28 +1100

    :rotating_light: lint fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,21 @@
 const Footer = () => (
   <footer className="text-sm text-gray-600 py-6">
     Powered by{" "}
-    <a href="https://openweathermap.org/" target="_blank" className="underline">
+    <a
+      href="https://openweathermap.org/"
+      target="_blank"
+      rel="noreferrer"
+      className="underline"
+    >
       Weather API
     </a>{" "}
     • Illustrations by{" "}
-    <a href="https://storyset.com/" target="_blank" className="underline">
+    <a
+      href="https://storyset.com/"
+      target="_blank"
+      rel="noreferrer"
+      className="underline"
+    >
       Storyset
     </a>
   </footer>
